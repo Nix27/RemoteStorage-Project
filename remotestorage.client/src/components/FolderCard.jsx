d4000d41ba4db52e1ea3346049d1d3fcfe31751b
Nix27 <nikolazecic2468@gmail.com>
2023-12-17 17:31:28 +0100

    App made responsive

--- remotestorage.client/src/components/FolderCard.jsx
+++ remotestorage.client/src/components/FolderCard.jsx
@@ -3,7 +3,7 @@ import { folderImage } from '../assets'
 
 const FolderCard = ({ folderName }) => {
   return (
-    <div className='flex flex-col w-[200px] h-[200px] p-3 rounded-[20px] transition-all hover:bg-blue-300 hover:cursor-pointer hover:scale-105'>
+    <div className='flex flex-col w-[150px] md:w-[180px] xl:w-[200px] h-[200px] p-3 rounded-[20px] transition-all hover:bg-blue-300 hover:cursor-pointer hover:scale-105'>
         <img src={folderImage} alt="folder image" />
         <div className='flex'>
             <span className='px-3 text-[1.1rem]'>{folderName}</span>
--- remotestorage.client/src/pages/FolderContainer.jsx
+++ remotestorage.client/src/pages/FolderContainer.jsx
@@ -12,7 +12,7 @@ const FolderContainer = () => {
 
   return (
     <div className='mx-5'>
-      <div className='flex flex-wrap gap-5'>
+      <div className='grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-5 xl:grid-cols-7 gap-3 md:gap-5'>
         {folders.length < 1 ? <p>Loading...</p> :
             folders.map(folder => (
               <Link key={crypto.randomUUID()} to={`files/${folder}`}>
